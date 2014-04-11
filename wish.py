c9b4c487e268c2d4f56e562e0b0542a3c91ffd06
anandkulkarni <anand@fairtradework.com>
2014-04-10 18:53:17 -0700

    wish.py
    first wish doc

---
+++ wish.py
@@ -0,0 +1,11 @@
+#writing a site scraper
+
+import scrapy
+
+#prompt the user for his name and keep it in a variable called name
+
+#prompt the user for the webpage where we're scraping records from
+
+#scrape all records from the webpage that contain the user's name
+
+#dump the results to screen
