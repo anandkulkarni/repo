210dc5010ca7c100b4f11bb97da66a423465ddf7
anandkulkarni <anand@fairtradework.com>
2014-04-10 19:02:20 -0700

    Update wish.py

--- wish.py
+++ wish.py
@@ -3,8 +3,10 @@
 import scrapy
 
 #prompt the user for his name and keep it in a variable called name
- 
+name = raw_input('Well, howdy. What is your name? ')
+
 #prompt the user for the webpage where we're scraping records from
+webpage = raw_input('What page do you wanna scrape?')
 
 #scrape all records from the webpage that contain the user's name
 
